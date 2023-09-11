19570f13b849a9eb9bfb54ba34dbad397f805166
Fadhlan Harashta <115058221+fadhlanharashta@users.noreply.github.com>
2023-09-11 09:39:04 +0700

    Update assignment1.cpp

--- assignment1.cpp
+++ assignment1.cpp
@@ -3,17 +3,15 @@
 #include <queue>
 #include <map>
 #include <limits>
-#include <algorithm> // Include this header for the reverse function
+#include <algorithm> 
 
 using namespace std;
 
-// Define a structure to represent edges in the graph
 struct Edge {
     char destination;
     int weight;
 };
 
-// Define a structure to represent a vertex and its distance from the source
 struct Vertex {
     char name;
     int distance;
@@ -23,31 +21,24 @@ struct Vertex {
     }
 };
 
-// Function to find the shortest path from 'start' to 'end' in the graph
 vector<char> dijkstra(map<char, vector<Edge>>& graph, char start, char end) {
-    // Create a priority queue to store vertices based on their distance
     priority_queue<Vertex, vector<Vertex>, greater<Vertex>> pq;
 
-    // Create a map to store the distance of each vertex from the start vertex
     map<char, int> distance;
     
-    // Create a map to store the previous vertex on the shortest path
     map<char, char> previous;
 
-    // Initialize distances and add the start vertex to the priority queue
     for (const auto& pair : graph) {
         char vertex = pair.first;
         distance[vertex] = (vertex == start) ? 0 : numeric_limits<int>::max();
         pq.push({vertex, distance[vertex]});
     }
 
-    // Dijkstra's algorithm
     while (!pq.empty()) {
         Vertex current = pq.top();
         pq.pop();
 
         if (current.name == end) {
-            // We have found the shortest path to 'end', so we can stop here
             break;
         }
 
@@ -61,7 +52,6 @@ vector<char> dijkstra(map<char, vector<Edge>>& graph, char start, char end) {
         }
     }
 
-    // Reconstruct the shortest path from 'start' to 'end'
     vector<char> shortestPath;
     char current = end;
     while (current != start) {
@@ -70,7 +60,6 @@ vector<char> dijkstra(map<char, vector<Edge>>& graph, char start, char end) {
     }
     shortestPath.push_back(start);
 
-    // Reverse the path to get it in the correct order
     reverse(shortestPath.begin(), shortestPath.end());
 
     return shortestPath;
@@ -93,10 +82,8 @@ int getTotalWeight(const vector<char>& path, const map<char, vector<Edge>>& grap
 
 
 int main() {
-    // Create the graph as an adjacency list
     map<char, vector<Edge>> graph;
     
-    // Add edges and their weights to the graph
     graph['a'] = {{'b', 1}, {'c', 3},{'d',5}};
     graph['b'] = {{'a', 1}, {'f', 4},{'e',7}};
     graph['c'] = {{'a', 3},{'f',1}};
